6434da2b0c2e809aae6c3ec06d70f482b08569fa
1620 <drgath@gmail.com>
2011-09-26 01:59:15 -0400

    API rewrite

--- api.js
+++ api.js
@@ -1,32 +1,39 @@
-var Embeditor = {
-    getInstance: function(id){
-      id = id || "embeditor";
-      /*return document.getElementById(id).contentWindow.postMessage({
+(function(exports){
+    
+    var callbacks = {};
+    
+    var Embeditor = function(id){
+        return {
+            getValue: function(fn){
+                callbacks['asd'] = fn;
+                document.getElementById(id).contentWindow.postMessage({
+                    message: "embeditor:getValue"
+                    cbid   : "asd",
+                }, "*");
+            },
+            setValue: function(val){
+                document.getElementById(id).contentWindow.postMessage({
+                    message: "embeditor:setValue",
+                    value  : val
+                }, "*")
+            }
+        }
+    }
+    
+    window.addEventListener("message", function(e){
+        switch(e.data.message) {
+            case "embeditor:ready":
+                // TODO
+                break;
+                
+            case "embeditor:sendValue":
+                callbacks[e.data.cbid](e.data.value);
+                delete callbacks[e.data.cbid];
+                break;
+        }
         
-      })*/
-    },
-    getValue: function(id, cb){
-        id = id || "embeditor";
-        Embeditor.recieveValue = cb;
-        document.getElementById(id).contentWindow.postMessage({
-            message: "embeditor:getValue"
-        }, "*")
-    },
-    setValue: function(id, val){
-        id = id || "embeditor";
-        document.getElementById(id).contentWindow.postMessage({
-            message: "embeditor:setValue",
-            value: val
-        }, "*")
-    }
-}
-
-window.addEventListener("message", function(e){
-    if (e.data.message === "embeditor:ready") {
-        Embeditor.onReady(e.windowName);   
-    }
-    else if (e.data.message === "embeditor:sendValue") {
-        //console.log(e.data.value);   
-        Embeditor.recieveValue(e.data.value);
-    }
-}, false);
+    }, false);
+    
+    exports.Embeditor = Embeditor;
+    
+})(window);
